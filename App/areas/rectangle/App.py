397ef40da17cc227c6f3673e3508757b07d905ad
tyobe <thasefa@yahoo.com>
2023-05-13 00:46:10 -0500

    added rectangle and triangle

---
+++ App/areas/rectangle/App.py
@@ -0,0 +1,9 @@
+# to calculate area of a rectangle
+# we need length
+# we need width
+# we multiply length by width
+
+length = 10
+width = 5
+area = length * width
+print("Area of a rectangle =",area)
---
+++ App/areas/triangle/App.py
@@ -0,0 +1,9 @@
+# to calculate a triangle 
+# get base
+# get height
+# to calculate area of a triangle multiply 1/2 base * height
+
+base = 10
+height = 20
+area = (base / 2)* height
+print ("Area of a triangle is",area)
