9329885ee583a567424877ea67e3890a18dd59b9
tyobe <thasefa@yahoo.com>
2023-05-14 23:41:43 -0500

    added area of a triangle

--- App/areas/triangle/App.py
+++ App/areas/triangle/App.py
@@ -3,7 +3,13 @@
 # get height
 # to calculate area of a triangle multiply 1/2 base * height
 
-base = 10
-height = 20
-area = (base / 2)* height
-print ("Area of a triangle is",area)
+# base = 10
+# height = 20
+# area = (base / 2)* height
+# print ("Area of a triangle is",area)
+
+userName = input ("Enter your full name:")
+base = input ("Enter your base :")
+height = input ("Enter your hieght:")
+areaOfATriangle = float(base)/2 * int(height)
+print(userName, "area of a triangle: ", areaOfATriangle)
